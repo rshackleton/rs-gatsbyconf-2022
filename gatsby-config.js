efd7103366d99aa6105febff343fd20bc30740a2
Richard Shackleton <rshackleton@live.co.uk>
2022-02-19 10:37:29 +0000

    Add prettier and format files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,28 @@
 module.exports = {
   siteMetadata: {
-      title: `rs-gatsbyconf-2022`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `rs-gatsbyconf-2022`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "images"
-  }]
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
 };
