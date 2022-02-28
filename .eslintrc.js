12423de526de723cdf704e8721f0646c868d0626
Richard Shackleton <rshackleton@live.co.uk>
2022-02-28 16:43:47 +0000

    Turn off no explicit any warnings for workshop

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
     '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
